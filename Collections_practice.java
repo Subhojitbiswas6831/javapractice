b1fed824c8492ab14f504bff0609a6e7ae796be6
Subhojit <Subhojitbiswas6831@gmail.com>
2023-10-03 11:25:25 +0530

    checking

--- Collections_practice.java
+++ Collections_practice.java
@@ -1,10 +1,29 @@
 package Framework;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 public class Collections_practice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Set<String > set = new HashSet<String>();
+		set.add("roop");
+		set.add("paul");
+		set.add("string");
+		
+		System.out.println(set);
+		
+		Iterator<String> i = set.iterator();
+		while(i.hasNext()) {
+		
+		System.out.println(i.next());
+		
+		
 
 	}
 
+}
 }
